c7c628f7677722b5d78e81acb34184410f67fa98
WangJingCan <jxtory@sina.com>
2018-11-01 19:11:10 +0800

    201811011911
    构建后台>后台功能;
    构建前台>前台页面;

--- Engine/application/index/controller/Allbase.php
+++ Engine/application/index/controller/Allbase.php
@@ -7,16 +7,29 @@ use think\Loader;
 
 class Allbase extends Controller
 {
-    // 各种定义
+    // *各种定义
+    // >应用名称
+    public $appName = "四分之三·区块链全球商业中心(大屏幕管理系统)";
     public function _initialize()
     {
         // 通用初始化
+
+        // 渲染全局变量
+        $this->AllAssign();
         
         // 创建上传中心目录
         // mkdirs("");
 
         // 创建配置目录
         // mkdirs("config");                       // 配置目录
+
+    }
+
+    // *公共或全局渲染
+    public function AllAssign()
+    {
+    	// >应用名称
+    	$this->assign("appName", $this->appName);
 
     }
 
