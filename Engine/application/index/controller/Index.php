e8aa99df8eeb69c59edb0bf6f91e261d41fce42f
Jahs <jxtory@sina.com>
2018-11-12 23:02:38 +0800

    201811122302
    上传检测，上传日志，生成缩略图

--- Engine/application/index/controller/Index.php
+++ Engine/application/index/controller/Index.php
@@ -19,13 +19,36 @@ class Index extends TflmsBase
             foreach ($files as $file) {
                 $mUR = 'myUploadRule';
                 $info = $file->rule($mUR)->move($this->upPath);
-                $data = [
-                    'cid'           =>  session("Uploader"),
-                    'fileUrl'       =>  $info->getSaveName(),
-                    'fileType'      =>  explode(".", $info->getSaveName())[1],
-                    'carousel'      =>  0
-                ];
-                db('files')->insert($data, true);
+
+                if($info){
+                    $data = [
+                        'cid'           =>  session("Uploader"),
+                        'fileUrl'       =>  $info->getSaveName(),
+                        'fileType'      =>  explode(".", $info->getSaveName())[1],
+                        'carousel'      =>  0
+                    ];
+                    $res = db('files')->insert($data, true);
+                    $companyName = db("invitation")->where("id", session("Uploader"))->find()['company'];
+
+                    if($res){
+                        $this->wLog("[用户行为]{$companyName}，上传了文件{$info->getSaveName()}，成功存储！");
+                    } else {
+                        $this->wLog("[用户行为]{$companyName}，上传了文件{$info->getSaveName()}，存储失败！");
+                    }
+
+                    $newPic = $this->image_resize(file_get_contents($this->upPath . DS . $info->getSaveName()), 384, 216);
+                    $fileName = explode(".", $info->getSaveName());
+
+                    if(file_put_contents($this->upPath . DS . $fileName[0] . "_thumb." . $fileName[1], $newPic)){
+                        $this->wLog("[系统行为]{$info->getSaveName()}，成功生成了缩略图。");
+                    } else {
+                        $this->wLog("[系统行为]{$info->getSaveName()}，成功缩略图失败了。");
+                    }
+
+                } else {
+                    $this->wLog("[用户行为]{$companyName}，有文件上传失败");
+                }
+
             }
 
             if($info){
