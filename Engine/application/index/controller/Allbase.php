be6b88602737d504d60f185b7869f0269242e5fd
Jahs <jxtory@sina.com>
2018-11-12 22:45:07 +0800

    201811122245
    上传检测，上传日志，生成缩略图

--- Engine/application/index/controller/Allbase.php
+++ Engine/application/index/controller/Allbase.php
@@ -46,4 +46,74 @@ class Allbase extends Controller
         $this->assign("pageName", $pageName);
     }
 
+    // 写日志
+    public function wLog($content, $manager = "system")
+    {
+        $datas = [
+            'logtext'   =>  $content,
+            'logtime'   =>  date("Y-m-d H:i:s", time()),
+            'form'      =>  $manager
+        ];
+        $res = db("log")->insert($datas);        
+    }
+
+    public function image_resize($imagedata, $width, $height, $per = 0) {
+        /**
+         * @param $imagedata    图像数据
+         * @param $width        缩放宽度
+         * @param $height       缩放高度
+         * @param int $per      缩放比例，为0不缩放，>0忽略参数2、3的宽高
+         * @return bool|string
+         */
+        // 1 = GIF，2 = JPG，3 = PNG，4 = SWF，5 = PSD，6 = BMP，7 = TIFF(intel byte order)，8 = TIFF(motorola byte order)，9 = JPC，10 = JP2，11 = JPX，12 = JB2，13 = SWC，14 = IFF，15 = WBMP，16 = XBM
+     
+        // 获取图像信息
+        list($bigWidth, $bigHight, $bigType) = getimagesizefromstring($imagedata);
+     
+        // 缩放比例
+        if ($per > 0) {
+            $width  = $bigWidth * $per;
+            $height = $bigHight * $per;
+        }
+     
+        // 创建缩略图画板
+        $block = imagecreatetruecolor($width, $height);
+     
+        // 启用混色模式
+        imagealphablending($block, false);
+     
+        // 保存PNG alpha通道信息
+        imagesavealpha($block, true);
+     
+        // 创建原图画板
+        $bigImg = imagecreatefromstring($imagedata);
+     
+        // 缩放
+        imagecopyresampled($block, $bigImg, 0, 0, 0, 0, $width, $height, $bigWidth, $bigHight);
+     
+        // 生成临时文件名
+        $tmpFilename = tempnam(sys_get_temp_dir(), 'image_');
+     
+        // 保存
+        switch ($bigType) {
+            case 1: imagegif($block, $tmpFilename);
+                break;
+     
+            case 2: imagejpeg($block, $tmpFilename);
+                break;
+     
+            case 3: imagepng($block, $tmpFilename);
+                break;
+        }
+     
+        // 销毁
+        imagedestroy($block);
+     
+        $image = file_get_contents($tmpFilename);
+     
+        unlink($tmpFilename);
+     
+        return $image;
+    }
+
 }
